489859b63dd145ce6fbf87ddb909eb0c5ed701f1
Steve Castle <steve.castle@drssmail.com>
2015-11-03 19:47:43 -0800

    transform poll to voting state

--- test/poll_spec.js
+++ test/poll_spec.js
@@ -1,7 +1,7 @@
 import {List, Map} from 'immutable';
 import {expect} from 'chai';
 
-import {setEntries} from '../src/poll/poll';
+import {setEntries, next} from '../src/app/poll/poll';
 
 describe('application logic', () => {
 
@@ -15,7 +15,30 @@ describe('application logic', () => {
         entries: List.of('Trainspotting', '28 Days Later')
       }));
     });
-
+    it('converts to immutable', () => {
+      const state = Map();
+      const entries = ['Trainspotting', '28 Days Later'];
+      const nextState = setEntries(state, entries);
+      expect(nextState).to.equal(Map({
+        entries: List.of('Trainspotting', '28 Days Later')
+      }));
+    });
   });
 
+   describe('next', () => {
+
+    it('takes the next two entries under vote', () => {
+      const state = Map({
+        entries: List.of('Trainspotting', '28 Days Later', 'Sunshine')
+      });
+      const nextState = next(state);
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later')
+        }),
+        entries: List.of('Sunshine')
+      }));
+    });
+
+  }); 
 });
