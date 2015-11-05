280d9def6ba28f2d96c7c2e53dd1dc40985a7f27
Steve Castle <steve.castle@drssmail.com>
2015-11-05 14:36:45 -0800

    completed voting sample app

---
+++ src/app/app.js
@@ -0,0 +1,3 @@
+import makeStore from './poll/store';
+
+export const store = makeStore();
--- src/app/core.js
+++ src/app/core.js
@@ -33,8 +33,10 @@ export function next(state) {
 
 export function vote(state, entry) {
 	return state.updateIn(
-		['vote', 'tally', entry],
+		['tally', entry],
 		0,
 		entryTally => entryTally + 1
 		);
 }
+
+export const INITIAL_STATE = Map();
