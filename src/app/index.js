758d06149b085336152a943afda3c26a158fcf74
Steve Castle <steve.castle@drssmail.com>
2015-11-19 15:17:39 -0800

    Mock data

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,10 @@
 import makeStore from './store';
 import startServer from './server';
-
+import data from '../../mock/entries.json'
 export const store = makeStore();
-store.dispatch({type:"SET_ENTRIES", entries: ['Trainspotting', '65DaysofStatic']});
+store.dispatch({
+	type:"SET_ENTRIES",
+	entries: require('../../mock/entries.json')
+});
 store.dispatch({type:"NEXT"})
 startServer(store);
